af09e59f5f568b09394c22cc28affbfe681a45c2
Tomoya Nagaya <24806125+toms3776c@users.noreply.github.com>
2022-08-04 23:12:45 +0900

    練習用コードの追加

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppHeder from './components/AppHeder.vue';
+import TodoList from './components/TodoList.vue';
+import { ref } from 'vue'
+
+const todos = ref([])
+const newTodo = ref('')
+
+const addTodo = () => {
+  todos.value.push(newTodo.value)
+  newTodo.value = ""
+}
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)
+}
+
+</script>
+
+<template>
+<AppHeder color="green">My Todo</AppHeder>
+<AppHeder color="gray"></AppHeder>
+<input type="text" size="30" v-model="newTodo">
+<button @click="addTodo()">追加</button>
+
+<TodoList v-bind:todos="todos" @removeTodo="removeTodo"></TodoList>
+
+</template>
